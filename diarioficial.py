ab0a208bba75772722ef3c191878929eb88dd672
Tiago Saboga <tiagosaboga@gmail.com>
2015-04-13 08:57:20 -0400

    diariooficial: refactor, add command-line options

--- diarioficial.py
+++ diarioficial.py
@@ -3,59 +3,147 @@
 import urllib.request
 import urllib.parse
 from bs4 import BeautifulSoup
+import argparse
+import datetime
 
-DEFAULT_ARGS = ("2015",
-                "10/04",
-                "10/04",
-                "null",
-                "1,1000,1010,1020,2,2000,3,3000,3020,4000,5000,6000,126,4,5,6,20,21,",
-                "",
-                "1",
-                "pesquisa_avancada",
-                "exteriores")
+DEFAULT_ARGS = {"ano": "2015",
+                "fim": "13/04",
+                "inicio": "10/04",
+                "fonetica": "null",
+                "jornal": "1,1000,1010,1020,2,2000,3,3000,3020,4000,5000,6000,126,4,5,6,20,21,",
+                "jornal_hidden": "",
+                "tipoPesquisa": "pesquisa_avancada",
+                "txtPesquisa": "exteriores remover"}
 
 class ImprensaNacional(object):
 
     def __init__(self, ano, fim, inicio,
-               fonetica, jornal, jornal_hidden,
-               paginaAtual, tipoPesquisa, txtPesquisa):
+                 fonetica, jornal, jornal_hidden,
+                 tipoPesquisa, txtPesquisa):
         self.url = "http://pesquisa.in.gov.br/imprensa/core/consulta.action"
-        self.search_params = [
-            ("edicao.ano", ano),
-            ("edicao.dtFim", fim),
-            ("edicao.dtInicio", inicio),
-            ("edicao.fonetica", fonetica),
-            ("edicao.jornal", jornal),
-            ("edicao.jornal_hidden", jornal_hidden),
-            ("edicao.paginaAtual", paginaAtual),
-            ("edicao.tipoPesquisa", tipoPesquisa),
-            ("edicao.txtPesquisa", txtPesquisa)]
+        # print(ano, fim, inicio, fonetica)
+        self.search_params = {
+            "edicao.ano":  ano,
+            "edicao.dtFim":  fim,
+            "edicao.dtInicio":  inicio,
+            "edicao.fonetica":  fonetica,
+            "edicao.jornal":  jornal,
+            "edicao.jornal_hidden":  jornal_hidden,
+            "edicao.paginaAtual":  "1",
+            "edicao.tipoPesquisa":  tipoPesquisa,
+            "edicao.txtPesquisa":  txtPesquisa}
+        self.results = self.search()
+        self.numberofresults = len(self.results)
 
-    def search(self):
-        postdata = urllib.parse.urlencode(self.search_params).encode('ascii')
-        pageread = urllib.request.urlopen(self.url, postdata).read()
-        page = pageread.decode("utf-8")
-        soup = BeautifulSoup(page[-5000:])
-        paginacao = soup.find(id="paginacao")
+    def processfirstpage(self, contents):
+        paginacao = contents.find(id="paginacao")
         # First substring of paginacao:
         results_description = next(paginacao.span.strings)
-
         # First word of results:
         word = results_description.split()[0]
-
         if word == "Nenhum":
-            self.numberofresults = 0
+            # print(contents)
+            numberofresults = 0
         else:
             try:
-                self.numberofresults = int(word)
+                numberofresults = int(word)
             except:
                 print("Error converting number of results: %s" % word)
+        return numberofresults
 
-imprensa = ImprensaNacional(*DEFAULT_ARGS)
 
-imprensa.search()
-print(imprensa.numberofresults)
+    def search(self):
+        contents = self.getcontents(1)
+        numberofresults = self.processfirstpage(contents)
+        if numberofresults == 0:
+            return []
 
+        results = self.parseresults(contents)
+        # print("Results - first page: %s." % results)
+        numberofpages = (numberofresults-1)//10
+        for page in range(1,numberofpages):
+            pagecontents = self.getcontents(page)
+            results.extend(self.parseresults(pagecontents))
+        return(results)
+
+    def getcontents(self, page=1):
+        search_params = self.search_params
+        search_params["edicao.paginaAtual"] = "%d" % page
+        postdata = urllib.parse.urlencode(search_params).encode('ascii')
+        pageread = urllib.request.urlopen(self.url, postdata).read()
+        page = pageread.decode("utf-8")
+        pagepart = page[page.index('html'):]
+        soup = BeautifulSoup(pagepart)
+        contents = soup.find(id="conteudo")
+        # print(contents)
+        return contents
+
+    def parseresults(self, contents):
+        """Parse results out of contents tree."""
+        results = []
+        targets = contents.findAll("th", class_="data")
+        excerpts = contents.findAll("td", class_="data")
+        for target, excerpt in zip(targets, excerpts):
+            link = target.a["href"]
+            link_description = target.getText()
+            excerpt = excerpt.getText()
+            results.append((link, link_description, excerpt))
+        return(results)
+
+
+def getoptions():
+    parser = argparse.ArgumentParser(description="Pesquisar no Diário Oficial.")
+    parser.add_argument('--datainicio', '-i', metavar="dd/mm[/yyyy]",
+                        help="Data inicial para busca.\nAtenção: o ano deve "
+                        "ser o mesmo para o início e para o fim. Caso os dois "
+                        "sejam definidos, será utilizado somente o do fim.")
+    parser.add_argument('--datafim', '-f', metavar="dd/mm[/yyyy]",
+                       help="Data final para busca.")
+    parser.add_argument('busca', nargs='+',
+                        help="Termos para busca.")
+    return parser.parse_args()
+    
+
+def normalize_date(date):
+    datelist = date.split('/')
+    if len(datelist) == 2:
+        datelist.append(datetime.date.today().year)
+    if len(datelist) == 3:
+        try:
+            day, month, year = [ int(x) for x in datelist ]
+        except:
+            print("Error converting date %s." % date)
+            raise
+    return ("%02d/%02d" % (day, month), "%4d" % year)
+    
+def main():
+    opts = getoptions()
+
+    if opts.datainicio:
+        inicio, ano = normalize_date(opts.datainicio)
+        DEFAULT_ARGS['inicio'] = inicio
+        DEFAULT_ARGS['ano'] = ano
+
+    if opts.datafim:
+        fim, ano = normalize_date(opts.datainicio)
+        DEFAULT_ARGS['fim'] = fim
+        DEFAULT_ARGS['ano'] = ano
+
+    if opts.busca:
+        DEFAULT_ARGS['txtPesquisa'] = ' '.join(opts.busca)
+        
+    imprensa = ImprensaNacional(**DEFAULT_ARGS)
+
+    messages = [ "Number of results: %d." % imprensa.numberofresults ]
+    for link, link_description, excerpt in imprensa.results:
+        message = "Link: %s.\n" % link
+        message += "Link description: %s.\n\n" % link_description
+        message += "Excerpt: %s." % excerpt
+        messages.append(message)
+    print("\n===========\n".join(messages))
+
+if __name__ == "__main__":
+    main()
 # This is how an empty page looks
 # <div id="paginacao">
 # <span>Nenhum item encontrado para a pesquisa solicitada.</span><br>
